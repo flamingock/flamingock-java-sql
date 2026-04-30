d07ee15279d943709985745d00dafbe9cc03a3a9
Antonio Perez Dieppa <aperezdieppa@gmail.com>
2026-04-30 19:12:51 +0100

    chore: keep flamingock test versio 1.2.0-beta.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ allprojects {
     version = "1.3.0-SNAPSHOT"
 
     extra["templateApiVersion"] = "1.3.3"
-    extra["flamingockVersion"] = "1.2.0"//for tests
+    extra["flamingockVersion"] = "1.2.0-beta.2"//for tests
 
     repositories {
         mavenLocal()
